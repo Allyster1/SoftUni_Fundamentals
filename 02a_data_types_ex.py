9e112476581f5fe4133247b1e94555ef1d124d26
Allyster <ovardovslav@gmail.com>
2025-02-16 00:52:14 +0200

    adding completed tasks

--- 02a_data_types_ex.py
+++ 02a_data_types_ex.py
@@ -73,3 +73,16 @@
 ##################################################
 # 07. Water Overflow
 #
+# fillings_count = int(input())
+# water_tank = 0
+#
+# for i in range(fillings_count):
+#     liters = int(input())
+#
+#     if water_tank + liters > 255:
+#         print("Insufficient capacity!")
+#         continue
+#     else:
+#         water_tank += liters
+#
+# print(water_tank)
