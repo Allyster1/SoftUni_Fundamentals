cdd413f9f6fef779d56a871b68d485a59a47cfcc
Allyster <ovardovslav@gmail.com>
2025-02-19 23:48:03 +0200

    adding completed tasks for List Basic Exercises

---
+++ 03a_lists_basic_ex.py
@@ -0,0 +1,37 @@
+# 01. Invert Values
+#
+# num_strings = input()
+#
+# numbers_list = num_strings.split(" ")
+# inverted_list = []
+#
+# for num_str in numbers_list:
+#     num = int(num_str)
+#     if num > 0:
+#         inverted_list.append(-abs(num))
+#     else:
+#         inverted_list.append(abs(num))
+#
+# print(inverted_list)
+#
+##################################################
+# 02. Multiple List
+#
+# factor = int(input())
+# count = int(input())
+#
+# num_list = []
+#
+# for i in range(1, count + 1):
+#     i *= factor
+#     num_list.append(i)
+#
+# print(num_list)
+#
+##################################################
+# 03. Football Cards
+#
+cards_sequence = input()
+
+team_a = 0
+team_b = 0
