deb5fd29be353f2dffb4a76ad2d28734fc28a40c
Allyster <ovardovslav@gmail.com>
2025-02-19 22:49:51 +0200

    adding completed tasks from Module Lists Basic

---
+++ 03_lists_basic.py
@@ -0,0 +1,46 @@
+# 01. Strange Zoo
+#
+# tail = input()
+# body = input()
+# head = input()
+#
+# my_list = [tail, body, head]
+#
+# my_list[0], my_list[2] = my_list[2], my_list[0]
+#
+# print(my_list)
+#
+##################################################
+# 02. COurses
+#
+# n = int(input())
+#
+# courses = []
+#
+# for _ in range(n):
+#     current_course = input()
+#     courses.append(current_course)
+#
+# print(courses)
+#
+##################################################
+# 03. List Statistics
+#
+# n = int(input())
+# positive_list = []
+# negative_list = []
+#
+# for _ in range(n):
+#     current_num = int(input())
+#
+#     if current_num >= 0:
+#         positive_list.append(current_num)
+#     else:
+#         negative_list.append(current_num)
+#
+# print(positive_list)
+# print(negative_list)
+# print(f"Count of positives: {len(positive_list)}")
+# print(f"Sum of negatives: {sum(negative_list)}")
+#
+##################################################
