7255ae5a6dd958069df4aee86f1d4df2d42723f7
Allyster <ovardovslav@gmail.com>
2025-02-22 13:26:25 +0200

    adding helper field

--- 03a_lists_basic_ex.py
+++ 03a_lists_basic_ex.py
@@ -35,3 +35,48 @@ cards_sequence = input()
 
 team_a = 0
 team_b = 0
+
+
+
+
+##################################################
+# Helper Field
+
+# Define list
+#my_list = [1,2,3,4,5]
+# my_list = ['a', 'b', 'c', 'd']
+
+# Display the index of the element in the list
+# index = my_list.index("a")
+# if "c" in my_list:
+#     num = my_list.index("c")
+#     print(num)
+#     print(my_list)
+# print(index)
+# for i in range(2, len(my_list)):
+#     print(my_list[i])
+
+# Sorting list
+# my_list.sort(reverse=True)
+# print(my_list)
+# my_list.reverse()
+# print(my_list)
+# my_list = my_list[::-1]
+# print(my_list)
+
+# Remove/Pop Element - remove(), removes the first encounter
+# num = my_list.pop()
+# print(my_list)
+#del is same as pop, just cannot return element
+# while "a" in my_list:
+#     my_list.remove("a")
+
+# Insert Element
+# index = 2
+# element = "apple"
+# my_list.insert(index, element)
+# print(my_list)
+
+# Count of same elements in a list
+# repetition = my_list.count(2)
+# print(repetition)
