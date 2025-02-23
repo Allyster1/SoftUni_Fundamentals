f4a5c7b1a524545f9e9c93a5a90b18c149026f64
Allyster <ovardovslav@gmail.com>
2025-02-24 00:21:09 +0200

    adding completed tasks

--- 03a_lists_basic_ex.py
+++ 03a_lists_basic_ex.py
@@ -55,21 +55,92 @@
 ##################################################
 # 04. Number Beggars
 #
-money_as_string = input().split(", ")
-beggars_count = int(input())
-
-money_as_integers = []
-
-for money in money_as_string:
-    money_as_integer.append(int(money))
-
-beggars_sums = 0
-
-
-
-
-
-
+# money_as_string = input().split(", ")
+# number_of_beggars = int(input())
+# money_as_integers = []
+#
+# for money in money_as_string:
+#     money_as_integers.append(int(money))
+#
+# beggars_sums = []
+# start_index = 0
+#
+# for current_beggar in range(number_of_beggars):
+#     current_beggar_sum = 0
+#     for current_index in range(start_index, len(money_as_integers), number_of_beggars):
+#         current_beggar_sum += money_as_integers[current_index]
+#     beggars_sums.append(current_beggar_sum)
+#     start_index += 1
+#
+# print(beggars_sums)
+#
+##################################################
+# 05. Faro Shuffle
+#
+# card_deck = input().split()
+# shuffle_count = int(input())
+#
+# for current_shuffle in range(shuffle_count):
+#     middle_deck = len(card_deck) // 2
+#
+#     left_part = card_deck[:middle_deck]
+#     right_part = card_deck[middle_deck:]
+#
+#     deck_after_shuffle = []
+#
+#     for card_index in range(len(right_part)):
+#         deck_after_shuffle.append(left_part[card_index])
+#         deck_after_shuffle.append(right_part[card_index])
+#     card_deck = deck_after_shuffle.copy()
+#
+# print(card_deck)
+#
+##################################################
+# 10. Bread Factory
+#
+# events = input().split("|")
+# total_energy = 100
+# total_coins = 100
+# is_open = True
+#
+# for event in events:
+#     e_item = event.split("-")
+#     e_type, e_val = e_item[0], int(e_item[1])
+#
+#     if e_type == "rest":
+#         initial_energy = total_energy
+#         total_energy += e_val
+#
+#         if total_energy > 100:
+#             total_energy = 100
+#
+#         gained_energy = total_energy - initial_energy
+#         print(f"You gained {gained_energy} energy.")
+#         print(f"Current energy: {total_energy}.")
+#     elif e_type == "order":
+#         if total_energy >= 30:
+#             total_coins += e_val
+#             total_energy -= 30
+#             print(f"You earned {e_val} coins.")
+#         else:
+#             total_energy += 50
+#             print("You had to rest!")
+#     else:
+#         if total_coins >= e_val:
+#             total_coins -= e_val
+#             print(f"You bought {e_type}.")
+#         else:
+#             is_open = False
+#             break
+#
+# if is_open:
+#     print("Day completed!")
+#     print(f"Coins: {total_coins}")
+#     print(f"Energy: {total_energy}")
+# else:
+#     print(f"Closed! Cannot afford {e_type}.")
+#
+#
 ##################################################
 # Helper Field
 
