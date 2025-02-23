91c0a67789841a56d409b147a47c5415581b8dd0
Allyster <ovardovslav@gmail.com>
2025-02-23 23:21:08 +0200

    adding complete tasks

--- 03a_lists_basic_ex.py
+++ 03a_lists_basic_ex.py
@@ -1,12 +1,10 @@
 # 01. Invert Values
 #
-# num_strings = input()
-#
-# numbers_list = num_strings.split(" ")
+# numbers_list = input().split(" ")
 # inverted_list = []
 #
-# for num_str in numbers_list:
-#     num = int(num_str)
+# for current_num in numbers_list:
+#     num = int(current_num)
 #     if num > 0:
 #         inverted_list.append(-abs(num))
 #     else:
@@ -31,10 +29,43 @@
 ##################################################
 # 03. Football Cards
 #
-cards_sequence = input()
+# my_list = [f"A-{number}" for number in range(1,12)]
+# my_list = [f"B-{number}" for number in range(1,12)]
+# team_a = ['A-1', 'A-2', 'A-3', 'A-4', 'A-5', 'A-6', 'A-7', 'A-8', 'A-9', 'A-10', 'A-11']
+# team_b = ['B-1', 'B-2', 'B-3', 'B-4', 'B-5', 'B-6', 'B-7', 'B-8', 'B-9', 'B-10', 'B-11']
+# is_terminated = False
+#
+# players = input().split()
+#
+# for player in players:
+#     # if the player is found in the team, remove him
+#     if player in team_a:
+#         team_a.remove(player)
+#     elif player in team_b:
+#         team_b.remove(player)
+#     # if a team has less than 7 players, break
+#     if len(team_a) < 7 or len(team_b) < 7:
+#         is_terminated = True
+#         break
+#
+# print(f"Team A - {len(team_a)}; Team B - {len(team_b)}")
+# if is_terminated:
+#     print("Game was terminated")
+#
+##################################################
+# 04. Number Beggars
+#
+money_as_string = input().split(", ")
+beggars_count = int(input())
 
-team_a = 0
-team_b = 0
+money_as_integers = []
+
+for money in money_as_string:
+    money_as_integer.append(int(money))
+
+beggars_sums = 0
+
+
 
 
 
