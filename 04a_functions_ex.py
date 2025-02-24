c1c4d5c89f5497ab68f4e4e76effe1fe45185247
Allyster <ovardovslav@gmail.com>
2025-02-24 20:29:20 +0200

    adding module Functions exercises

---
+++ 04a_functions_ex.py
@@ -0,0 +1,33 @@
+# 01. Smallest of Three Numbers
+#
+# n1 = int(input())
+# n2 = int(input())
+# n3 = int(input())
+#
+# smallest_int = min(n1,n2,n3)
+# print(smallest_int)
+#
+##################################################
+# 02. add and subtract
+#
+# def add_and_subtact(num1, num2, num3):
+#     addition_sum = num1 + num2
+#     total_sum = addition_sum - num3
+#
+#     return total_sum
+#
+# n1 = int(input())
+# n2 = int(input())
+# n3 = int(input())
+#
+# result = add_and_subtact(n1,n2,n3)
+#
+# print(result)
+#
+##################################################
+
+
+
+
+#
+##################################################
