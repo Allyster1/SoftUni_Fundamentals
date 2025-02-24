99c055bcb8ee156d7ffa49a5d9a47f122f97bba8
Allyster <ovardovslav@gmail.com>
2025-02-24 18:19:02 +0200

    removing white spaces

--- 04_functions.py
+++ 04_functions.py
@@ -96,22 +96,18 @@
 ##################################################
 # 07. Rounding
 #
-def round_numbers(nums):
-    rounded_list = []
-    for n in nums:
-        num = round(float(n))
-        rounded_list.append(num)
-    return rounded_list
-
-input_str = input().split()
-result = round_numbers(input_str)
-
-print(result)
-
-
-
-
-
+# def round_numbers(nums):
+#     rounded_list = []
+#     for n in nums:
+#         num = round(float(n))
+#         rounded_list.append(num)
+#     return rounded_list
+#
+# input_str = input().split()
+# result = round_numbers(input_str)
+#
+# print(result)
+#
 ##################################################
 #
 # helper Field
