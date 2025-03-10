08bf119680420176bc58d2c2e5951b4c854671ed
Allyster <ovardovslav@gmail.com>
2025-03-11 00:50:44 +0200

    updating instance attribute to class attribute

--- 06a_Objects_and_Classes_Exercises/05_Class.py
+++ 06a_Objects_and_Classes_Exercises/05_Class.py
@@ -1,13 +1,14 @@
 class Class:
+    __students_count = 22
+
     def __init__(self, name):
         self.name = name
         self.students = []
         self.grades = []
-        self.__students_count = 22
 
     def add_student(self, name: str, grade: float):
-        if self.__students_count > 0:
-            self.__students_count -= 1
+        if Class.__students_count > 0:
+            Class.__students_count -= 1
             self.students.append(name)
             self.grades.append(grade)
 
